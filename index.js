68e751fd5b490bc906b5554c8a7628edec6605a5
Luis Martinez <luis.martinez@newfold.com>
2023-11-19 12:33:59 -0400

    update: add scheme picker

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const textElement = document.getElementById("text");
 const hoursElement = document.getElementById("hour_hand");
 const MinutesElement = document.getElementById("minute_hand");
 const secondsElement = document.getElementById("second_hand");
+const colorPickerElement = document.getElementById("colorPicker");
 
 let showDate = true;
 
@@ -26,3 +27,52 @@ requestAnimationFrame(animate);
 textElement.addEventListener("click", () => {
   showDate = !showDate;
 });
+
+colorPickerElement.addEventListener("input", updateColorScheme);
+
+function updateColorScheme(event) {
+  const newMainColor = event.target.value;
+
+  document.documentElement.style.setProperty("--main-color", newMainColor);
+  document.documentElement.style.setProperty(
+    "--background-color",
+    lightenDarkenColor(newMainColor, -20)
+  );
+  document.documentElement.style.setProperty(
+    "--text-color-primary",
+    lightenDarkenColor(newMainColor, 50)
+  );
+  document.documentElement.style.setProperty(
+    "--text-color-secondary",
+    lightenDarkenColor(newMainColor, 30)
+  );
+  document.documentElement.style.setProperty(
+    "--accent-color",
+    lightenDarkenColor(newMainColor, -40)
+  );
+}
+
+function lightenDarkenColor(col, amt) {
+  let usePound = false;
+
+  if (col[0] === "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  const num = parseInt(col, 16);
+
+  let r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  let b = ((num >> 8) & 0x00ff) + amt;
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  let g = (num & 0x0000ff) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
